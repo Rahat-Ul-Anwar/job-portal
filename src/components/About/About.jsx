3b8e18864d0cdf211fbd8c37ca76f1b0c9e484a7
Rahat-Ul-Anwar <anwarrahat69@gmail.com>
2023-02-18 16:21:36 +0600

    something channges

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h2>This is About</h2>
+        <div className='mt-6'>
+            <h2 className='text-3xl text-center font-bold'>This is About</h2>
         </div>
     );
 };
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div>
-            <h2>This is Contact us</h2>
+        <div className='mt-6'>
+            <h2 className='text-3xl text-center font-bold'>This is Contact us</h2>
         </div>
     );
 };
