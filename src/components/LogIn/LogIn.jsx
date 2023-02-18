dba1fe45b18d32c0ef31db8ee37871e2f15ab247
Rahat-Ul-Anwar <anwarrahat69@gmail.com>
2023-02-18 14:22:45 +0600

    dark theme added

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const LogIn = () => {
   return (
@@ -30,9 +31,9 @@ const LogIn = () => {
                 className="input input-bordered"
               />
               <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
+                <Link  className="label-text-alt link link-hover">
                   Forgot password?
-                </a>
+                </Link>
               </label>
             </div>
             <div className="form-control mt-6">
